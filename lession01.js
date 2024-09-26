b79b08139ea1deb415811deae2bdcb24bd16d9ee
DucAnh <anhnguyen2k373@gmail.com>
2024-09-26 20:19:48 +0700

    biến

--- lession01.js
+++ lession01.js
@@ -1,1 +1,15 @@
-alert("Hello python")
+// alert("Hello python");
+
+// 1. Biến
+var number= 10;
+let full_name = "DucAnh";
+const age = 21; 
+
+// giá trị quyết định mang kiểu dữ liệu nào
+// phạm vi hoạt động của biến
+// const không gán lại giá trị mới 
+
+
+number; 
+full_name;
+age;
