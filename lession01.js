98fb44b8030ac19cdb5b1ff1684c343b68fdc4d6
DucAnh <anhnguyen2k373@gmail.com>
2024-09-26 20:26:31 +0700

    chuẩn đặt tên biến

--- lession01.js
+++ lession01.js
@@ -2,7 +2,7 @@
 
 // 1. Biến
 var number= 10;
-let full_name = "DucAnh";
+let fullName = "DucAnh";
 const age = 21; 
 
 // giá trị quyết định mang kiểu dữ liệu nào
@@ -17,4 +17,6 @@ age = 25;
 // lỗi tại dòng 15 do gán lại biến do gán lại biến cho giá trị const
 // Uncaught TypeError: Assignment to constant variable.
 // at lession01.js:15:5
+
+
 
