45bbc6bf0145df058a83ea7b82c355df9737a432
DucAnh <anhnguyen2k373@gmail.com>
2024-10-01 19:10:20 +0700

    Phân tích

--- learn2/lession01.js
+++ learn2/lession01.js
@@ -4,9 +4,15 @@ console.log("Vòng lặp hoạt động hàng ngày");
 //  i là biến chạy (giá trị khởi tạo): biến kiểm soát vòng lặp, biến khởi tạo
 //  điều kiện kết thúc vòng lặp
 //  bước nhảy
-for(let i = 1; i <= 7; i = i+1){
-    console.log(i);
-    console.log("Thức dậy");
-    console.log("Hoạt động");
-    console.log("Đi ngủ");    
+// for(let i = 1; i <= 7; i = i+1){
+//     console.log(i);
+//     console.log("Thức dậy");
+//     console.log("Hoạt động");
+//     console.log("Đi ngủ");    
+// }
+
+for(let i = 1; i <=7; i = i + 1){
+    console.log(`So sánh i=1 và điều kiện i <= ${i} => true => thực hiện khối lệnh trong vòng for`);
 }
+
+
