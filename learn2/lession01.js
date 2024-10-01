5fcfa59e9577033e2f71399b914d1f60664cebce
DucAnh <anhnguyen2k373@gmail.com>
2024-10-01 19:22:19 +0700

    Thay đổi bước nhảy i = i + 2

--- learn2/lession01.js
+++ learn2/lession01.js
@@ -15,8 +15,20 @@ console.log("Vòng lặp hoạt động hàng ngày");
 //     console.log(`So sánh i= ${i} và điều kiện i <= 7 (${i} <=7)  => true => thực hiện khối lệnh trong vòng for`);
 // }
 
-for(let i = 0; i < 7; i = i + 1){
-    console.log(`So sánh i= ${i} và điều kiện i < 7 (${i} < 7)  => true => thực hiện khối lệnh trong vòng for`);
+// for(let i = 0; i < 7; i = i + 1){
+//     console.log(`So sánh i= ${i} và điều kiện i < 7 (${i} < 7)  => true => thực hiện khối lệnh trong vòng for`);
+// }
+
+
+for(let i = 1; i <= 14; i = i+2){
+    console.log(i);
+    console.log("Thức dậy");
+    console.log("Hoạt động");
+    console.log("Đi ngủ");    
 }
+
+
+
+
 
 
