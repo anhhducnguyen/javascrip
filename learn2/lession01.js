377f35f2cefc9b99adbc20204ccaf151f612ade2
DucAnh <anhnguyen2k373@gmail.com>
2024-10-01 18:55:01 +0700

    Vòng lặp hoạt động hàng ngày

---
+++ learn2/lession01.js
@@ -0,0 +1,8 @@
+console.log("Vòng lặp hoạt động hàng ngày");
+
+for(let i = 1; i <= 7; i = i+1){
+    console.log(i);
+    console.log("Thức dậy");
+    console.log("Hoạt động");
+    console.log("Đi ngủ");    
+}
