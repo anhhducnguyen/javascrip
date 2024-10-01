6fdd5a16eceae22baf20e4841a060d90ea5c6d44
DucAnh <anhnguyen2k373@gmail.com>
2024-10-01 19:02:37 +0700

    Ghi chú cho vòng lặp

--- learn2/lession01.js
+++ learn2/lession01.js
@@ -1,5 +1,9 @@
 console.log("Vòng lặp hoạt động hàng ngày");
 
+//  Bộ 3 diều kiện
+//  i là biến chạy (giá trị khởi tạo): biến kiểm soát vòng lặp, biến khởi tạo
+//  điều kiện kết thúc vòng lặp
+//  bước nhảy
 for(let i = 1; i <= 7; i = i+1){
     console.log(i);
     console.log("Thức dậy");
