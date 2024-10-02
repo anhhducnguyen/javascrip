dee398d0882499ccf6d72720dc79cbb5d622a89a
DucAnh <anhnguyen2k373@gmail.com>
2024-10-03 06:43:09 +0700

    Bài 1: Xây dựng trò chơi đoán số

--- Week2/bai1.js
+++ Week2/bai1.js
@@ -1,14 +1,31 @@
-result = 5
-let loop = true
+// result = 5
+// let loop = true
 
-while(loop){
-    let n = +prompt("Nhập vào số n (1-10):");
+// while(loop){
+//     let n = +prompt("Nhập vào số n (1-10):");
+//     if(n > result) {
+//         console.log("Lớn hơn");
+//     } else if (n < result) {
+//         console.log("Nhỏ hơn");
+//     } else {
+//         console.log("Great");
+//         break;
+//     }
+// }
+
+
+result = 5;
+let n;
+
+do {
+    n = +prompt("Nhập vào số n (1-10):");
+
     if(n > result) {
         console.log("Lớn hơn");
     } else if (n < result) {
         console.log("Nhỏ hơn");
     } else {
         console.log("Great");
-        break
+        break;
     }
-}
+} while (n !== result);
