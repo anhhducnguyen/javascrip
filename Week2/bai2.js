cbd0e0c6de0f8f7dca1fe6f03e5cab1a08f55b15
DucAnh <anhnguyen2k373@gmail.com>
2024-10-03 06:53:04 +0700

    Bài 2: In bảng cửu chương

---
+++ Week2/bai2.js
@@ -0,0 +1,8 @@
+for(let i = 2; i < 10; i++) {
+    console.log(`Bảng nhân ${i}`);
+    for (let j = 1; j < 10; j++) {
+        let result = i * j;
+        console.log(`${i} * ${j} = ${result}`);
+    }
+    console.log("----------");
+}
