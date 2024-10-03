51f56d0a52773e70e7842f9639b889298a9fdb94
DucAnh <anhnguyen2k373@gmail.com>
2024-10-03 19:43:10 +0700

    Duyệt phần tử trong mảng

---
+++ learn3/lession01.js
@@ -0,0 +1,57 @@
+let studentLists = [
+    "Nguyen Duc Anh", 
+    "Nguyen Mai Ngan", 
+    "Hoang Ngoc Huyen",
+    "Hoang Ngoc Mai",
+    "Nguyen Ngoc Lan"
+];
+
+// element: phần tử
+// index
+// length
+
+console.log("Danh sách sinh viên",studentLists);
+
+// templateString_read_one
+console.log(`${studentLists[2]} đi ăn không`);
+
+// read_all thao tac or duyet qua cac phan tu trong mang
+console.log("Duyệt qua các phần tử trong mảng");
+
+for(let i = 0; i < studentLists.length; i++) {
+    console.log(`${i+1} Hay lam bai tap di ${studentLists[i]}`);
+}
+
+console.log("Duyệt qua các phần tử trong mảng từ giữa về cuối");
+
+// Duyệt từ giữa về cuối
+mid = parseInt(studentLists.length/2);
+for(let i = mid; i < studentLists.length; i++) {
+    console.log(`${i+1} Hay lam bai tap di ${studentLists[i]}`);
+}
+
+// For_of: duyệt qua mảng không quan tâm đến chỉ số từng phần tử trong mảng
+console.log("For_of: duyệt qua mảng không quan tâm đến chỉ số từng phần tử trong mảng");
+
+for (let student of studentLists) {
+    console.log(`2. Hay lam bai tap di ${student}`);
+}
+
+// For_in: duyệt qua mảng theo chiều xuôi và vẫn thao tác được với từng phần tử
+for (let index in studentLists) {
+    console.log(+index + 1, studentLists[index]);
+}
+// Thêm vào đầu
+studentLists.unshift("Nguyen Van Dau")
+
+// Thêm vào cuối
+studentLists.push("Nguyen Van Cuoi");
+
+// Thêm vị trí bất kì 
+studentLists.splice()
+
+
+
+
+
+
