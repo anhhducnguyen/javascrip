f9e42e64e52fb81c0c54117bd23348a8ec6fd63d
DucAnh <anhnguyen2k373@gmail.com>
2024-10-03 19:46:33 +0700

    Create

--- learn3/lession01.js
+++ learn3/lession01.js
@@ -10,45 +10,53 @@ let studentLists = [
 // index
 // length
 
-console.log("Danh sách sinh viên",studentLists);
+// console.log("Danh sách sinh viên",studentLists);
 
-// templateString_read_one
-console.log(`${studentLists[2]} đi ăn không`);
+// // templateString_read_one
+// console.log(`${studentLists[2]} đi ăn không`);
 
-// read_all thao tac or duyet qua cac phan tu trong mang
-console.log("Duyệt qua các phần tử trong mảng");
+// // read_all thao tac or duyet qua cac phan tu trong mang
+// console.log("Duyệt qua các phần tử trong mảng");
 
-for(let i = 0; i < studentLists.length; i++) {
-    console.log(`${i+1} Hay lam bai tap di ${studentLists[i]}`);
-}
+// for(let i = 0; i < studentLists.length; i++) {
+//     console.log(`${i+1} Hay lam bai tap di ${studentLists[i]}`);
+// }
 
-console.log("Duyệt qua các phần tử trong mảng từ giữa về cuối");
+// console.log("Duyệt qua các phần tử trong mảng từ giữa về cuối");
 
-// Duyệt từ giữa về cuối
-mid = parseInt(studentLists.length/2);
-for(let i = mid; i < studentLists.length; i++) {
-    console.log(`${i+1} Hay lam bai tap di ${studentLists[i]}`);
-}
+// // Duyệt từ giữa về cuối
+// mid = parseInt(studentLists.length/2);
+// for(let i = mid; i < studentLists.length; i++) {
+//     console.log(`${i+1} Hay lam bai tap di ${studentLists[i]}`);
+// }
 
-// For_of: duyệt qua mảng không quan tâm đến chỉ số từng phần tử trong mảng
-console.log("For_of: duyệt qua mảng không quan tâm đến chỉ số từng phần tử trong mảng");
+// // For_of: duyệt qua mảng không quan tâm đến chỉ số từng phần tử trong mảng
+// console.log("For_of: duyệt qua mảng không quan tâm đến chỉ số từng phần tử trong mảng");
 
-for (let student of studentLists) {
-    console.log(`2. Hay lam bai tap di ${student}`);
-}
+// for (let student of studentLists) {
+//     console.log(`2. Hay lam bai tap di ${student}`);
+// }
 
-// For_in: duyệt qua mảng theo chiều xuôi và vẫn thao tác được với từng phần tử
-for (let index in studentLists) {
-    console.log(+index + 1, studentLists[index]);
-}
+// // For_in: duyệt qua mảng theo chiều xuôi và vẫn thao tác được với từng phần tử
+// for (let index in studentLists) {
+//     console.log(+index + 1, studentLists[index]);
+// }
 // Thêm vào đầu
 studentLists.unshift("Nguyen Van Dau")
+console.log(studentLists);
+
 
 // Thêm vào cuối
 studentLists.push("Nguyen Van Cuoi");
+console.log(studentLists);
+
 
 // Thêm vị trí bất kì 
-studentLists.splice()
+studentLists.splice(4, 0, "Nguyen Van Giua");
+console.log(studentLists);
+
+
+
 
 
 
