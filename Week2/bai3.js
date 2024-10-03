225df3dd1da2b7592ce2daa6616bed06925fa232
DucAnh <anhnguyen2k373@gmail.com>
2024-10-03 15:20:47 +0700

    In số nguyên tố

---
+++ Week2/bai3.js
@@ -0,0 +1,23 @@
+let input = +prompt("Nhập vào số n:");
+
+let isPrime = false;  // Khai báo biến isPrime bên ngoài để tránh lỗi
+
+if (input < 2) {
+    console.log(`${input} không phải số nguyên tố`);
+} else {
+    isPrime = true;  // Giả sử số đó là nguyên tố
+    let check = Math.sqrt(input);
+
+    for (let i = 2; i <= check; i++) {
+        if (input % i === 0) {
+            isPrime = false;  // Tìm thấy ước số, không phải nguyên tố
+            break;
+        }
+    }
+}
+
+if (isPrime) {
+    console.log(`${input} là số nguyên tố`);
+} else {
+    console.log(`${input} không phải số nguyên tố`);
+}   
