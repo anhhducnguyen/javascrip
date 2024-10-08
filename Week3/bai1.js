f5f70ab322b1f6b8ebe58f222079ed18053f80a7
DucAnh <anhnguyen2k373@gmail.com>
2024-10-08 17:13:14 +0700

    Tìm mảng con thỏa mãn điều kiện

---
+++ Week3/bai1.js
@@ -0,0 +1,26 @@
+let arr = [1, 5, 6, 3, 2, 1, 5, 6, 7];
+
+for (let i = 0; i < arr.length; i++) {
+    list = [];
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] == arr[j]) {
+            list.push(arr[i]);
+        }
+    }
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i]);
+    }
+}
+
+console.log("test");
+
+for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+for (const element of object) {
+    
+}
