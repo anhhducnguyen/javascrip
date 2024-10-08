648ba8387d120a515cd44358c6776f73c8a2a966
DucAnh <anhnguyen2k373@gmail.com>
2024-10-08 17:23:48 +0700

    Sắp xếp mảng theo yêu cầu

---
+++ Week3/bai7.js
@@ -0,0 +1,14 @@
+// Yêu cầu người dùng nhập chuỗi
+let str = prompt("Nhập vào một chuỗi bất kỳ: ");
+let substrings = [];  // Mảng chứa các chuỗi con
+
+// Duyệt qua các vị trí trong chuỗi
+for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j <= str.length; j++) {
+        // Thêm chuỗi con từ vị trí i đến j vào mảng
+        substrings.push(str.slice(i, j));
+    }
+}
+
+// In ra mảng chứa các chuỗi con
+console.log(substrings);
